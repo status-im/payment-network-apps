ca8e952ec3814cb2f896368d5614376875689f29
Michele Balistreri <michele@bitgamma.com>
2019-12-05 14:13:59 +0300

    use EIP-712 data and signatures

--- network-contracts/contracts/KeycardWallet.sol
+++ network-contracts/contracts/KeycardWallet.sol
@@ -1,9 +1,22 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract KeycardWallet {
   event TopUp(address from, uint256 value);
   event NewPaymentRequest(uint256 nonce, address to, uint256 value);
   event NewWithdrawal(address to, uint256 value);
+
+  uint256 constant chainId = 1;
+
+  struct Payment {
+    uint256 nonce;
+    uint256 amount;
+    address to;
+  }
+
+  bytes32 constant EIP712DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+  bytes32 constant PAYMENT_TYPEHASH = keccak256("Payment(uint256 nonce,uint256 amount,address to)");
+  bytes32 DOMAIN_SEPARATOR;
 
   address public owner;
   bytes3 public name;
@@ -28,6 +41,14 @@ contract KeycardWallet {
   }
 
   constructor(bytes3 _name, address _keycard, uint256 _maxTxValue) public {
+    DOMAIN_SEPARATOR = keccak256(abi.encode(
+      EIP712DOMAIN_TYPEHASH,
+      keccak256("KeycardWallet"),
+      keccak256("1"),
+      chainId,
+      address(this)
+    ));
+
     owner = msg.sender;
     name = _name;
     keycard = _keycard;
@@ -44,17 +65,26 @@ contract KeycardWallet {
     settings.maxTxValue = _maxTxValue;
   }
 
-  function recover(bytes32 hash, bytes memory sig) public pure returns(address) {
-    require(sig.length == 65, "bad signature length");
+  function hash(Payment memory _payment) internal pure returns (bytes32) {
+    return keccak256(abi.encode(
+      PAYMENT_TYPEHASH,
+      _payment.nonce,
+      _payment.amount,
+      _payment.to
+    ));
+  }
+
+  function verify(Payment memory _payment, bytes memory _sig) internal view returns (bool) {
+    require(_sig.length == 65, "bad signature length");
 
     bytes32 r;
     bytes32 s;
     uint8 v;
 
     assembly {
-      r := mload(add(sig, 32))
-      s := mload(add(sig, 64))
-      v := byte(0, mload(add(sig, 96)))
+      r := mload(add(_sig, 32))
+      s := mload(add(_sig, 64))
+      v := byte(0, mload(add(_sig, 96)))
     }
 
     if (v < 27) {
@@ -63,30 +93,29 @@ contract KeycardWallet {
 
     require(v == 27 || v == 28, "signature version doesn't match");
 
-    return ecrecover(hash, v, r, s);
+    bytes32 digest = keccak256(abi.encodePacked(
+        "\x19\x01",
+        DOMAIN_SEPARATOR,
+        hash(_payment)
+    ));
+
+    return ecrecover(digest, v, r, s) == keycard;
   }
 
-  function requestPayment(bytes32 _hashToSign, bytes memory _signature, uint256 _nonce, address payable _to, uint256 _value) public {
+  function requestPayment(Payment memory _payment, bytes memory _signature) public {
     // check that a keycard address has been set
     require(keycard != address(0), "keycard address not set");
 
-    // check that the _hashToSign has been produced with the nonce, to, and value
-    bytes32 message = keccak256(abi.encodePacked(_nonce, _to, _value));
-    bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-    bytes32 expectedHash = keccak256(abi.encodePacked(prefix, message));
-    require(expectedHash == _hashToSign, "signed params are different");
-
-    // check that the _hashToSign has been signed by the keycard
-    address signer = recover(_hashToSign, _signature);
-    require(signer == keycard, "signer is not the keycard");
+    // verify the signer
+    require(verify(_payment, _signature), "signer is not the keycard");
 
     // check that the nonce is valid
-    require(nonce == _nonce, "invalid nonce");
+    require(nonce == _payment.nonce, "invalid nonce");
 
-    // check that _value is not greater than settings.maxTxValue
-    require(_value <= settings.maxTxValue, "amount not allowed");
+    // check that _payment.amount is not greater than settings.maxTxValue
+    require(_payment.amount <= settings.maxTxValue, "amount not allowed");
 
-    int256 availableBalance = int256(address(this).balance - totalPendingWithdrawals - _value);
+    int256 availableBalance = int256(address(this).balance - totalPendingWithdrawals - _payment.amount);
     // check that balance is enough for this payment
     require(availableBalance >= 0, "balance is not enough");
 
@@ -94,10 +123,10 @@ contract KeycardWallet {
     nonce++;
 
     // add pendingWithdrawal
-    totalPendingWithdrawals += _value;
-    pendingWithdrawals[_to] += _value;
+    totalPendingWithdrawals += _payment.amount;
+    pendingWithdrawals[_payment.to] += _payment.amount;
 
-    emit NewPaymentRequest(_nonce, _to, _value);
+    emit NewPaymentRequest(_payment.nonce, _payment.to, _payment.amount);
   }
 
   function withdraw() public {
